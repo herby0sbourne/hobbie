9e851b567cdf7519ae23f0981a59c53f0a4661f0
Herby <herbyosbourne31@gmail.com>
2022-10-26 10:09:38 -0500

    ctore index state in context

--- src/provider/Store.jsx
+++ src/provider/Store.jsx
@@ -1,22 +1,28 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
 const initial = {
-  result: [],
+    result: [],
+    index: {
+        startIdx: 0,
+        stopIdx: 2
+    }
 };
 
 export const StoreContext = createContext(initial);
 
-const StoreProvider = ({children}) => {
-  const [result, setResult] = useState([]);
-  useEffect(() => {
-    console.log(result)
-  }, [result]);
+const StoreProvider = ({ children }) => {
+    const [result, setResult] = useState([]);
+    const [index, setIndex] = useState({ startIdx: 0, stopIdx: 2 });
 
-  return (
-      <StoreContext.Provider value={{result, setResult}}>
-        {children}
-      </StoreContext.Provider>
-  );
+    useEffect(() => {
+        console.log(result);
+    }, [result]);
+
+    return (
+        <StoreContext.Provider value={{ result, index, setResult, setIndex }}>
+            {children}
+        </StoreContext.Provider>
+    );
 };
 
 StoreContext.displayName = "Hobby";
