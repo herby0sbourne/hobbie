6c6128bababe5bf06757a9cceaf6a81bf93830f5
Herby <herbyosbourne31@gmail.com>
2022-10-26 10:07:55 -0500

    set state to default on homePage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../provider/Store";
 
 const Home = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { setIndex, setResult } = useContext(StoreContext);
 
-  return (
-    <div className="home">
-      <h2>Pick a Hobby</h2>
-      <button
-        className="btn"
-        onClick={() => {
-          navigate("hobbies");
-        }}
-      >
-        Start a new session
-      </button>
-    </div>
-  );
+    useEffect(() => {
+        setResult([]);
+        setIndex({ startIdx: 0, stopIdx: 2 });
+    }, []);
+
+    return (
+        <div className="home">
+            <h2>Pick a Hobby</h2>
+            <button
+                className="btn"
+                onClick={() => {
+                    navigate("hobbies");
+                }}
+            >
+                Start a new session
+            </button>
+        </div>
+    );
 };
 
 export default Home;
