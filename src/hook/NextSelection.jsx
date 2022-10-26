644b6dcb1c1b21a6f45d6cd57c380fb63d233cfd
Herby <herbyosbourne31@gmail.com>
2022-10-26 10:08:34 -0500

    create NextSelection hook

--- src/hook/NextSelection.jsx
+++ src/hook/NextSelection.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from 'react';
+import { StoreContext } from '../provider/Store';
 
 const useNext = () => {
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(2);
+  const { index, setIndex } = useContext(StoreContext);
+  const { startIdx: start, stopIdx: end } = index;
+  // const [start, setStart] = useState(0);
+  // const [end, setEnd] = useState(2);
 
   const next = () => {
-    setStart((prevState) => {
-      return prevState + 2;
-    });
-
-    setEnd((prevState) => {
-      return prevState + 2;
+    setIndex((prevState) => {
+      return {
+        startIdx: prevState.startIdx + 2,
+        stopIdx: prevState.stopIdx + 2,
+      };
     });
   };
+
+  // const next = () => {
+  //   setStart((prevState) => {
+  //     return prevState + 2;
+  //   });
+
+  //   setEnd((prevState) => {
+  //     return prevState + 2;
+  //   });
+  // };
 
   return { start, end, next };
 };
