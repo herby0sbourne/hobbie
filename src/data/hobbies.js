fa11d7ee16b0a27a3c88685dc815126865cf7271
Herby <herbyosbourne31@gmail.com>
2022-10-27 22:32:50 -0500

    added more hobbies

--- src/data/hobbies.js
+++ src/data/hobbies.js
@@ -1,24 +1,44 @@
 const HOBBIES = [
-  {
-    id: "1",
-    title: "Gardening",
-    url: "https://media.istockphoto.com/photos/farmer-hands-take-care-and-protect-young-little-sprout-plant-in-the-picture-id1161321111?k=20&m=1161321111&s=612x612&w=0&h=EuUGp7Ed7G7ggUqSGbeIn7SFnAfyab0TzXmdTRnlVzk=",
-  },
-  {
-    id: "2",
-    title: "Painting",
-    url: "https://www.thesprucecrafts.com/thmb/4krlR_ONU4EDXKbCb8FCWFkz--Y=/2121x1414/filters:fill(auto,1)/GettyImages-922707682-5b90467bc9e77c0025931eef.jpg",
-  },
-  {
-    id: "3",
-    title: "Cooking",
-    url: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_41/3044956/191009-cooking-vegetables-al-1422.jpg",
-  },
-  {
-    id: "4",
-    title: "Gaming",
-    url: "https://www.accenture.com/t00010101T000000Z__w__/au-en/_acnmedia/Accenture/Redesign-Assets/DotCom/Images/Global/Thumbnail768x432/27/Accenture-Gaming-Featured-768x432.jpg",
-  },
+    {
+        id: "1",
+        title: "Gardening",
+        url: "https://media.istockphoto.com/photos/farmer-hands-take-care-and-protect-young-little-sprout-plant-in-the-picture-id1161321111?k=20&m=1161321111&s=612x612&w=0&h=EuUGp7Ed7G7ggUqSGbeIn7SFnAfyab0TzXmdTRnlVzk="
+    },
+    {
+        id: "2",
+        title: "Painting",
+        url: "https://www.thesprucecrafts.com/thmb/4krlR_ONU4EDXKbCb8FCWFkz--Y=/2121x1414/filters:fill(auto,1)/GettyImages-922707682-5b90467bc9e77c0025931eef.jpg"
+    },
+    {
+        id: "3",
+        title: "Cooking",
+        url: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_41/3044956/191009-cooking-vegetables-al-1422.jpg"
+    },
+    {
+        id: "4",
+        title: "Gaming",
+        url: "https://www.accenture.com/t00010101T000000Z__w__/au-en/_acnmedia/Accenture/Redesign-Assets/DotCom/Images/Global/Thumbnail768x432/27/Accenture-Gaming-Featured-768x432.jpg"
+    },
+    {
+        id: "5",
+        title: "Swimming",
+        url: "https://cdn4.explainthatstuff.com/og-large-swimming.jpg"
+    },
+    {
+        id: "6",
+        title: "Drawing",
+        url: "https://pyxis.nymag.com/v1/imgs/160/7b8/be9b71674a92129013428120b1a0c699b7-howtogetintodrawing.rsocial.w1200.jpg"
+    },
+    {
+        id: "7",
+        title: "Programming",
+        url: "https://www.codingem.com/wp-content/uploads/2021/10/juanjo-jaramillo-mZnx9429i94-unsplash-scaled.jpg"
+    },
+    {
+        id: "8",
+        title: "Sewing",
+        url: "https://images.immediate.co.uk/production/volatile/sites/32/2019/08/Which-sewing-machine-thread-do-i-need-eb13270.jpg"
+    }
 ];
 
 export default HOBBIES;
